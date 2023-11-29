5ae564d3bf99612aa0f63bc33ea3c2a946d27e66
nocive.science <nocive.science@gmail.com>
2023-11-29 05:20:26 -0300

    terminado con el handlebars

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const {engine} = require('express-handlebars');
 const app = express();
 const db = require('./db');
 const port = 3000;
+app.set('views', './src/views');
+app.engine('hbs', engine({
+    extname: '.hbs',
+    defaultLayout: 'index.hbs',
+    layoutsDir: './src/views/layouts',
+    partialsDir: './src/views/partials'
+}));
+app.set('view engine', 'hbs');
 app.get('/', async(request, response) => {
     try{
       const query = await db.query('SELECT * FROM cuadernos');
-      response.json(query.rows);
+      response.render('home', {cuadernos: query.rows});
     }catch(err){
       console.log(err);
     };
