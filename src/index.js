d066ba8b31cde831bcc75835399cf1752ec48c0d
nocive.science <nocive.science@gmail.com>
2023-11-29 05:09:08 -0300

    terminado con handlebars incluido

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const {engine} = require('express-handlebars');
 const db = require('./db');
 const app = express();
 const port = 3000;
+app.set('views', './src/views');
+app.engine('hbs', engine({
+    extname: 'hbs',
+    defaultLayout: 'default',
+}));
+app.set('view engine', 'hbs');
 app.get('/data', async(req, res) => {
     const datos = await db.query('SELECT * FROM cuadernos');
-    res.json(datos.rows);
+    res.render('inicio', {datos: datos.rows});
 });
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
