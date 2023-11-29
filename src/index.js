ae9283aba97535a55e0c302ed1d2bef721f0a54f
nocive.science <nocive.science@gmail.com>
2023-11-29 03:51:34 -0300

    ahora funciona la base

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const db = require('./db');
+const {engine} = require('express-handlebars');
+const port = 3000;
+app.use(express.json());
+app.set('views', './src/views');
+app.engine('.hbs', engine({
+  defaultLayout: 'main',
+  extname: '.hbs',
+}));
+app.set('view engine', '.hbs');
+app.get('/', (req, res) => {
+  const datas = db.query('SELECT * FROM cuadernos', (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('home', {datas: result.rows});
+    }
+  });
+})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
