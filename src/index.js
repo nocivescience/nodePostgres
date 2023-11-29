650cdef3c0f6d2c0e9a13e6d0ac8bf41c83f0da8
nocive.science <nocive.science@gmail.com>
2023-11-29 04:52:32 -0300

    proyecto terminado en el hadlebars

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 const db = require('./db');
 const express = require('express');
+const {engine}= require('express-handlebars');
 const app = express();
 const port = 3000;
-app.get('/data', async(req, res) => {
-    const respuesta = await db.query('SELECT * FROM cuadernos');
-    res.json(respuesta.rows);
+app.set('views', './src/views');
+app.engine('hbs', engine({
+  extname: 'hbs',
+  defaultLayout: 'principal',
+}));
+app.set('view engine', 'hbs');
+app.get('/', async(req, res) => {
+    try{
+        const result = await db.query('SELECT * FROM cuadernos');
+        res.render('home', {resultado: result.rows});
+    }catch(e){
+        console.log(e);
+    }
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
