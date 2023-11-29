b73eb2267abce0f870b9fb1884010280828ecb02
nocive.science <nocive.science@gmail.com>
2023-11-29 02:19:47 -0300

    terminado rama_4

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 const express = require('express');
 const app = express();
+const db = require('./db');
+const port = 3000;
+app.get('/', async(request, response) => {
+    try{
+      const query = await db.query('SELECT * FROM cuadernos');
+      response.json(query.rows);
+    }catch(err){
+      console.log(err);
+    };
+});
+app.use(express.json());
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+});
