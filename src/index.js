a7cb7a340c5b1aedeef9dc4bb88bb8f81dbd21a6
nocive.science <nocive.science@gmail.com>
2023-11-29 21:27:59 -0300

    terminado y funcionando el formulario y le base de datos

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ const db = require('./db');
 const express = require('express');
 const {engine} = require('express-handlebars');
 const app = express();
-const {Pool} = require('pg');
 const port = 3000;
 app.set('views', './src/views');
 app.engine('.hbs', engine({
   extname: '.hbs',
   defaultLayout: 'main',
 }));
+app.use(express.urlencoded({extended: true}));
 app.set('view engine', '.hbs');
 app.get('/', async(req, res)=>{
     try{
@@ -18,7 +18,18 @@ app.get('/', async(req, res)=>{
         console.log(e);
         res.status(500).send('Something went wrong');
     }
-}); 
+});
+app.post('/formulario', async(req, res)=>{
+    const {asignatura, nota, titulo}= req.body;
+    try{
+        await db.query('INSERT INTO cuadernos (asignatura, nota, titulo) VALUES ($1, $2, $3)', [asignatura, nota, titulo]);
+        res.redirect('/');
+        console.log('insertado');
+    }catch(error){
+        console.log(`mira el error: ${error}`);
+        res.status(500).send('Something went wrong');
+    }
+});
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
