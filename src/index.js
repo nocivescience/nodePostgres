2e4bc229c2d41dc6ecdbcf13383660dbbff89930
nocive.science <nocive.science@gmail.com>
2023-11-29 21:52:40 -0300

    termninado con formulario y base de datos corriendo

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const app = express();
 const db = require('./db');
 const port = 3000;
 app.set('views', './src/views');
+app.use(express.urlencoded({extended: false}));
 app.engine('hbs', engine({
     extname: '.hbs',
     defaultLayout: 'index.hbs',
@@ -18,6 +19,16 @@ app.get('/', async(request, response) => {
     }catch(err){
       console.log(err);
     };
+});
+app.post('/formulario', async(request, response) => {
+    try{
+        const {asignatura, nota, titulo}= request.body;
+        await db.query('INSERT INTO cuadernos (asignatura, nota, titulo) VALUES ($1, $2, $3)', [asignatura, nota, titulo]);
+        response.redirect('/');
+        console.log(`Se ha añadido el cuaderno de ${asignatura} con la nota ${nota} y el título ${titulo}`);
+    }catch(err){
+      console.log(`Sobreviví al error: ${err}`);
+    }
 });
 app.use(express.json());
 app.listen(port, () => {
