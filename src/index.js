0697cd7a6695ac2dd8707459bcdc32b691274483
nocive.science <nocive.science@gmail.com>
2023-11-29 21:45:06 -0300

    termniado y funcionando el formulario y la base de datos

--- src/index.js
+++ src/index.js
@@ -8,10 +8,21 @@ app.engine('hbs', engine({
     extname: 'hbs',
     defaultLayout: 'default',
 }));
+app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'hbs');
-app.get('/data', async(req, res) => {
+app.get('/', async(req, res) => {
     const datos = await db.query('SELECT * FROM cuadernos');
     res.render('inicio', {datos: datos.rows});
+});
+app.post('/formulario', async(req, res) => {
+    const {asignatura, nota, titulo} = req.body;
+    try{
+        await db.query('INSERT INTO cuadernos (asignatura, nota, titulo) VALUES ($1, $2, $3)', [asignatura, nota, titulo]);
+        res.redirect('/');
+        console.log('Datos insertados');
+    }catch(e){
+        console.log(`Error feliz: ${e}`);
+    }
 });
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
